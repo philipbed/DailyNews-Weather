16ebfd37a9a789c6bd56179e0fe979187c52ffbc
Philip Bedward Jr <philipbed813@gmail.com>
2016-05-15 10:42:06 -0400

    added more comments

--- fetchWeather.js
+++ fetchWeather.js
@@ -1,28 +1,100 @@
-
+/**
+ * @author: Philip Bedward
+ */
 $(document).ready(function(){
+  // Make a new weather object
   var weatherapp = new Weather("NY","Rochester");
-  weatherapp.retrieve();
+
+  /** @see retrieve() */
+  var forecast = weatherapp.retrieve();
+
+  /**
+   * Uses Deferred on the retrieve call in order to
+   * only call parseDayWeather when the retrieve method is done.
+   *
+   * @param: forecast {JSON Object} - the deferred retrieve method call
+   * @param: weatherForecast {JSON Object} -  is the week's forecast in json.
+   */
+  $.when(forecast).done(function(weatherForecast){
+      weatherapp.parseDayWeather(weatherForecast.simpleforecast);
+  });
+
+
 
 
 });
 
+/**
+ * Weather object responsible for making the initial ajax call
+ * To Wunderground's API service. Will use this to get the forecast.
+ *
+ * @constructor
+ * @param {state} the State to pull weather from
+ * @param {city} the city to pull weather from
+ */
 function Weather(state,city){
-  this.apiKey = "6f6e312e456560e4";
   this.stateCode = state;
   this.cityName = city;
-  this.apiLink = "http://api.wunderground.com/api/"+this.apiKey+"/forecast/q/"+this.stateCode+"/"+this.cityName+".json",
 
+  /** @private */
+  var apiKey = "6f6e312e456560e4";
+
+
+  /**
+   * This link can be formatted to find the weather in different cities and different states.
+   * You can also change the type of forecast it returns
+   * (e.g 10hourday - the 10 hour forecast for that day).
+   * Right now it is set to the 5 days forecast. (Section: "/'forecast'/")
+   * @private
+  */
+  this.apiLink = "http://api.wunderground.com/api/"+apiKey+"/forecast/q/"+this.stateCode+"/"+this.cityName+".json",
+
+  /**
+   * This function will make the call to retrieve the 5 day forecast
+   * for Rochester, NY.
+   *
+   * @return {JSON Object} - the weather information as json
+   */
   this.retrieve = function(){
-    $.ajax({
-      type: 'GET',
-      url: this.apiLink,
+    // Declare Deferred to enforce synchronization
+    var deferred = $.Deferred();
+    var dailyForecast;
 
-      success: function(result){
-        console.log("We're OK");
+    /**
+     * this method is called when the ajax call is
+     * done gathering the information that we need
+     */
+    function sendForecast(){
+
+      // resolve/fulfill our promise and send the dailyForecast
+      deferred.resolve(dailyForecast);
+
+    }
+    jqxhr = $.ajax({
+      type: 'GET',
+      contentType: 'application/x-www-form-urlencoded',
+      url: this.apiLink,
+      dataType: "JSON",
+      success: function(data){
+        return data
       },
-      error: function(result){
-        console.error(result);
-      },
+    }).done(function(){
+      dailyForecast = jqxhr.responseJSON.forecast;
+
+      // fulfill the promise we made
+      sendForecast();
+
     });
+    // promises that this method is not finished and we
+    // will handle some type of action when it is done.
+    return deferred.promise();
+  };
+
+  /**
+   * This function will handle parsing through the weather for a day.
+   * @param weather {JSON Object} -
+   */
+  this.parseDayWeather = function(weather){
+    console.log(weather);
   };
 };
