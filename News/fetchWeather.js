b58f5609d4c9e9bc44cc9e46b8ad4bd85b4a7249
Philip Bedward Jr <philipbed813@gmail.com>
2016-05-15 09:45:12 -0400

    Revert "Added some comments and made the retrieve method deffered."
    This reverts commit 743d89cf3a22827615d57d66e09f3ce7f480c9c9.

--- News/fetchWeather.js
+++ News/fetchWeather.js
@@ -1,85 +1,28 @@
 
 $(document).ready(function(){
-
   var weatherapp = new Weather("NY","Rochester");
-  var forecast = weatherapp.retrieve();
-  $.when(forecast).done(function(weatherForecast){
-      console.log("finished loading");
-      weatherapp.parseDayWeather(weatherForecast.simpleforecast);
-  });
-
-
+  weatherapp.retrieve();
 
 
 });
 
-/**
- * Weather object responsible for making the initial ajax call
- * To Wunderground's API service. Will use this to get the forecast.
- *
- * @constructor
- * @param {state} the State to pull weather from
- * @param {city} the city to pull weather from
- */
 function Weather(state,city){
+  this.apiKey = "6f6e312e456560e4";
   this.stateCode = state;
   this.cityName = city;
-
-  /** @private */
-  var apiKey = "6f6e312e456560e4";
-
-
-  /**
-   * This link can be formatted to find the weather in different cities and different states.
-   * You can also change the type of forecast it returns
-   * (e.g 10hourday - the 10 hour forecast for that day).
-   * Right now it is set to the 5 days forecast. (Section: "/'forecast'/")
-   * @private
-  */
-  this.apiLink = "http://api.wunderground.com/api/"+apiKey+"/forecast/q/"+this.stateCode+"/"+this.cityName+".json",
-
-  /**
-   * This function will make the call to retrieve the 5 day forecast
-   * for Rochester, NY.
-   *
-   * @return {JSON Object}
-   */
-
+  this.apiLink = "http://api.wunderground.com/api/"+this.apiKey+"/forecast/q/"+this.stateCode+"/"+this.cityName+".json",
 
   this.retrieve = function(){
-    console.log("retrieve");
-
-    var deferred = $.Deferred();
-    var dailyForecast;
-    function callback(){
-      deferred.resolve(dailyForecast);
-
-    }
-    jqxhr = $.ajax({
+    $.ajax({
       type: 'GET',
-      contentType: 'application/x-www-form-urlencoded',
       url: this.apiLink,
-      dataType: "JSON",
-      success: function(data){
-        console.log("success");
-        return data
+
+      success: function(result){
+        console.log("We're OK");
       },
-    }).done(function(){
-      console.log("done");
-      dailyForecast = jqxhr.responseJSON.forecast;
-      callback();
-
+      error: function(result){
+        console.error(result);
+      },
     });
-
-    return deferred.promise();
-  };
-
-  /**
-   * This function will handle parsing through the weather for a day.
-   *
-   */
-  this.parseDayWeather = function(weather){
-    console.log("parse Weather");
-    console.log(weather);
   };
 };
