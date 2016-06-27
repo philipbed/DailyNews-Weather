ff2f10b4ae62515c8b6bab456da3b591b35d281d
Philip Bedward Jr <philipbed813@gmail.com>
2016-06-26 23:01:59 -0400

    learned how to use the carousel, will replace placeholder images with new cards

--- js/fetchWeather.js
+++ js/fetchWeather.js
@@ -22,6 +22,9 @@ $(document).ready(function(){
 
 
 
+  
+
+
 
 
 
--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ $(document).ready(function(){
   });
 
 
+    // $(document).ready(function(){
+    //   $('.carousel').carousel();
+    // });
+
 
 });
 
