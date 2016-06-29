97dfde6cd14199cdc9af8b6380364ef245a6f0c3
Philip Bedward Jr <philipbed813@gmail.com>
2016-06-29 01:14:51 -0400

    made loading external html cleaner

--- js/main.js
+++ js/main.js
@@ -3,13 +3,25 @@ $(document).ready(function(){
   $("#weatherTab").click(function(){
 
     // readHtmlFile("file:///C:/Users/Computer%20User/Documents/News/weather.html");
-    $("#tab1").load("weather_copy.html");
+    $.get("weather_copy.html",function(data){
+      var scripts = $(data).filter("script");
+      $
+      scripts.each(function(){
+        var script = $(this);
+        $("body").append(script);
+      })
+
+      var weather = $(data).filter(".carousel");
+
+      $("#tab1").append(weather);
+      $('.carousel').carousel({
+              dist:0,
+              shift:0,
+              padding:300,
+
+        });
+    },"html");
   });
-
-
-    // $(document).ready(function(){
-    //   $('.carousel').carousel();
-    // });
 
 
 });
