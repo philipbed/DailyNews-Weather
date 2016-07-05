fe359f733c627c5d82a45e59535d7668f1a675ae
Philip Bedward Jr <philipbed813@gmail.com>
2016-07-05 08:39:42 -0400

    Formatted the news card more, still needs work

--- js/fetchData.js
+++ js/fetchData.js
@@ -56,27 +56,30 @@ $(document).ready(function (){
 
       // add image only if it is ABC's default
       if(image.toLowerCase().indexOf("default") == -1){
-        console.log(image);
+        //console.log(image);
         var img = "<img class='image newsImg' src=" +image+">";
         card.append("<div class='card-image waves-effect waves-block waves-light'>"+img+"</div>");
+        card.append("<div class='card-content newsTitle'><span class='card-title grey-text text-darken-4'>"+title+"</span></div>");
+      }
+      else{
+        card.append("<span class='card-title grey-text text-darken-4'>"+title+"</span>");
+        continue;
       }
       console.log(card);
       // add title and description
 
-      card.append("<div class='card-content newsTitle'></div>");
-      $(".newsTitle").eq(i).append("<span class='activator'><span class='card-title activator grey-text text-darken-4'>"+title+"<i class='material-icons right activator'>more_vert</i></span></span>");
+
+      // $(".newsTitle").eq(i).append("<span class='card-title grey-text text-darken-4'>"+title+"</span>");
       // card.append("<div class='card-reveal'>"+desc+"</div>");
       //
       //
-      // // add link to full story
-      //
-      // var linkText = "Click Here";
-      // var remainingLinkText = "To Read The Full Story";
-      // var linkClass = 'navLink';
-      // var listItem = "<li><a class="+linkClass+" href="+link+" target='_blank'>"+linkText+"</a>"+remainingLinkText+"</li>";
-      // card.append("<div class="+linkClass+" 'mdl-card__actions mdl-card--border'> \
-      // <a class='mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect' \
-      // href="+link+" target='_blank '>"+linkText+" </a>"+remainingLinkText+"</div><br>");
+      // add link to full story
+
+      var linkText = "Click Here";
+      var remainingLinkText = "To Read The Full Story";
+      var linkClass = 'navLink';
+      var listItem = "<a class='waves-effect waves-light btn' "+linkClass+" href="+link+" target='_blank'>"+linkText+"</a>"+remainingLinkText;
+      card.append("<br><div class="+linkClass+" 'card-action'>"+listItem+"</div>");
 
     }
   }
